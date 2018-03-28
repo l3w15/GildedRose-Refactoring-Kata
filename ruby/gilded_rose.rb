3e08a541866d2b99eb26db99db47b71bf8ba9a5d
Lewis Watson <lewwwiswatson@gmail.com>
2018-03-28 14:37:42 +0100

    :recycle: Use previous tests to test new Classes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,62 +1,24 @@
 class GildedRose
 
+  # GET_ITEM_TYPE = { MaturingItem => "Aged Brie" }
+
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
+      # item = GET_ITEM_TYPE.key(item.name).new(item.name, item.sell_in, item.quality) #{ |k, v| v.include?(item.name) }
+      # puts item
+      # item = type.new(item.name, item.sell_in, item.quality)
+      # puts "item is " + item
 
-      return item = item if sulfuras?(item)
+      item = item if sulfuras?(item)
       item.sell_in = item.sell_in - 1
-      return increase_quality(item) if aged_brie?(item)
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-
-            item.quality = item.quality - 1
-
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-
-                item.quality = item.quality - 1
-
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
-    puts "after "
-    @items.each do |item|
-      puts item
+      MaturingItem::update_item if aged_brie?(item)
+      increase_quality(item) if backstage?(item) && item.sell_in > 10
+      backstage_quality(item) if backstage?(item)
+      decrease_quality(item)
     end
   end
 
@@ -73,29 +35,20 @@ class GildedRose
   end
 
   def increase_quality(item)
-    return item.quality = item.quality + 1 if item.quality < 50
+    item.quality += 1 if item.quality < 50
   end
 
+  def backstage_quality(item)
+    item.quality += 2 if item.sell_in > 5 && item.quality < 50
+    item.quality += 3 if item.sell_in >= 0 && item.quality < 50
+    item.quality = 0
+  end
 
+  def decrease_quality(item)
+    return item.quality -= 2 if item.quality > 0 && item.name.downcase.include?("conjured")
+    return item.quality = item.quality - 1 if item.quality > 0
+  end
 end
-
-# let(:sulfuras) { double("item1", @name = "Sulfuras, Hand of Ragnaros", @sell_in = 30, @quality = 50) }
-# let(:aged_brie) { double("item2", :name => "Aged Brie", :sell_in => 1, :quality => 0) }
-# let(:aged_brie_quality_50) { double("item3", :name => "Aged Brie", :sell_in => 30, :quality => 50) }
-# let(:normal_item) { double("item4", @name = "foo", @sell_in = 11, @quality = 50) }
-# let(:normal_item_quality_0) { double("item5", :name => "bar", :sell_in => 0, :quality => 0) }
-# let(:backstage) { double("item6", :name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 20, :quality => 0) }
-# let(:backstage_10_days) { double("item7", :name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 10, :quality => 0) }
-# let(:backstage_5_days) { double("item8", :name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 5, :quality => 0) }
-# let(:backstage_0_days) { double("item9", :name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 0, :quality => 50) }
-# let(:backstage_quality_50) { double("item10", :name => "Backstage passes to a TAFKAL80ETC concert", :sell_in => 20, :quality => 50) }
-#
-# before(:each) do
-#   items = [ sulfuras, aged_brie, aged_brie_quality_50, normal_item, normal_item_quality_0,
-#     backstage, backstage_0_days, backstage_5_days, backstage_10_days, backstage_quality_50 ]
-#   gild_rose = GildedRose.new(items)
-#   gild_rose.update_quality()
-# end
 
 class Item
   attr_accessor :name, :sell_in, :quality
@@ -108,5 +61,54 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class BaseItem < Item
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+
+  def age_item
+    @sell_in -= 1
+  end
+
+  def update_item
+    age_item
+    @quality -= 1 if @quality > MIN_QUALITY
+  end
+end
+
+class MaturingItem < BaseItem
+  def update_item
+    age_item
+    @quality += 1 if @quality < MAX_QUALITY
+  end
+end
+
+class LegendaryItem < Item
+    def update_item
+    end
+end
+
+class DeadlineItem < BaseItem
+  DOUBLE_QUALITY_DAYS = 10
+  TRIPLE_QUALITY_DAYS = 5
+  LAST_DAY = 0
+
+  def update_item
+    age_item
+    @sell_in > LAST_DAY ? quality_add_one : @quality = 0
+  end
+
+  def quality_add_one
+    @sell_in > DOUBLE_QUALITY_DAYS && @quality < MAX_QUALITY - 1 ? @quality += 1 : quality_add_two
+  end
+
+  def quality_add_two
+    @sell_in > TRIPLE_QUALITY_DAYS && @quality < MAX_QUALITY - 2 ? @quality += 2 : quality_add_three
+  end
+
+  def quality_add_three
+    @quality += 3 if @sell_in > LAST_DAY && @quality < MAX_QUALITY - 3 
   end
 end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -11,75 +11,75 @@ describe GildedRose do
 
     describe "Sulfuras" do
       it "never changes" do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 30, 50)
-        GildedRose.new([item]).update_quality()
-        expect(item).to have_attributes(:sell_in => 30, :quality => 50)
+        item = LegendaryItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
+        item.update_item
+        expect(item).to have_attributes(:sell_in => 0, :quality => 80 )
       end
     end
 
     describe "a normal item" do
       it "ages and decreases in quality" do
-        item = Item.new("ham", 11, 50)
-        GildedRose.new([item]).update_quality()
+        item = BaseItem.new("ham", 11, 50)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 10, :quality => 49)
       end
 
       it "never has quality of below zero" do
-        item = Item.new("beef", 0, 0)
-        GildedRose.new([item]).update_quality()
+        item = BaseItem.new("beef", 0, 0)
+        item.update_item
         expect(item).to have_attributes(:sell_in => -1, :quality => 0)
       end
     end
 
     describe "Aged Brie" do
       it "increases in quality the older it gets" do
-        item = Item.new("Aged Brie", 1, 0)
-        GildedRose.new([item]).update_quality()
+        item = MaturingItem.new("Aged Brie", 1, 0)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 0, :quality => 1)
       end
 
       it "never has quality over 50" do
-        item = Item.new("Aged Brie", 0, 50)
-        GildedRose.new([item]).update_quality()
+        item = MaturingItem.new("Aged Brie", 0, 50)
+        item.update_item
         expect(item).to have_attributes(:sell_in => -1, :quality => 50)
       end
     end
 
     describe "Backstage Pass" do
       it "increases in quality the older it gets" do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 0)
-        GildedRose.new([item]).update_quality()
+        item = DeadlineItem.new("Backstage passes to a TAFKAL80ETC concert", 20, 0)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 19, :quality => 1)
       end
 
       it "increases by 2 within 10 days" do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
-        GildedRose.new([item]).update_quality()
+        item = DeadlineItem.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 9, :quality => 2)
       end
 
       it "increases by 2 within 5 days" do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
-        GildedRose.new([item]).update_quality()
+        item = DeadlineItem.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 4, :quality => 3)
       end
 
       it "drops to zero after the concert" do
-        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)
-        GildedRose.new([item]).update_quality()
+        item = DeadlineItem.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)
+        item.update_item
         expect(item).to have_attributes(:sell_in => -1, :quality => 0)
       end
 
       it "never has quality over 50" do
-        item = Item.new("Aged Brie", 10, 50)
-        GildedRose.new([item]).update_quality()
+        item = DeadlineItem.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+        item.update_item
         expect(item).to have_attributes(:sell_in => 9, :quality => 50)
       end
     end
 
     describe "#sulfuras?" do
       it "returns true if the item is Sulfuras" do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 30, 50)
+        item = Item.new("Sulfuras, Hand of Ragnaros", 30, 80)
         expect(GildedRose.new([item]).sulfuras?(item)).to eq(true)
       end
 
@@ -95,7 +95,7 @@ describe GildedRose do
         expect(GildedRose.new([item]).aged_brie?(item)).to eq(true)
       end
 
-      it "returns false if the item is not Sulfurus" do
+      it "returns false if the item is not Aged Brie" do
         item = Item.new("foo", 30, 50)
         expect(GildedRose.new([item]).aged_brie?(item)).to eq(false)
       end
@@ -107,7 +107,7 @@ describe GildedRose do
         expect(GildedRose.new([item]).backstage?(item)).to eq(true)
       end
 
-      it "returns false if the item is not Sulfurus" do
+      it "returns false if the item is not backstage pass" do
         item = Item.new("foo", 30, 50)
         expect(GildedRose.new([item]).backstage?(item)).to eq(false)
       end
@@ -120,25 +120,33 @@ describe GildedRose do
       end
     end
 
+    describe "#backstage_quality" do
+      it "adds two to passes with sell_in between 10 and 5 days" do
+        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+        expect(GildedRose.new([item]).backstage_quality(item)).to eq(2)
+      end
 
+      it "adds three to passes with sell_in between 5 and 0 days" do
+        item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+        expect(GildedRose.new([item]).backstage_quality(item)).to eq(3)
+      end
+
+      it "sets the quality to zero if the concert day has passed" do
+        item = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 50)
+        expect(GildedRose.new([item]).backstage_quality(item)).to eq(0)
+      end
+    end
+
+    describe "#decrease_quality" do
+      it "decreases normal items\' quality by one" do
+        item = Item.new("ham", 1, 1)
+        expect(GildedRose.new([item]).decrease_quality(item)).to eq(0)
+      end
+
+      it "decreases conjured items\' quality by two" do
+        item = Item.new("Conjured ham", 1, 2)
+        expect(GildedRose.new([item]).decrease_quality(item)).to eq(0)
+      end
+    end
   end
-  #
-  # describe "check behaviour of all items" do
-  #   it "doesn't delete items" do
-  #     items = [
-  #       Item.new("Sulfuras, Hand of Ragnaros", 40, 50),
-  #       Item.new("Aged Brie", 20, 20),
-  #       Item.new("Test no negative", -20, 0),
-  #       Item.new("Aged Brie", 10, 50),
-  #       Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 50),
-  #       Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30),
-  #       Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30),
-  #       Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 30)
-  #     ]
-  #     GildedRose.new(items).update_quality();
-  #     expect(items.length).to eq(8);
-  #   end
-  # end
-  #
-
 end
