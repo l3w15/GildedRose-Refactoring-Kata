8e50921d68ec791abcc89e6c2b08530bbf58702c
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-01 19:13:28 +0100

    :recycle: Pass tests using @updated_items and @item_type in gilded_rose.rb

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -5,28 +5,30 @@ require './conjured_item'
 
 class GildedRose
 
-  ITEM_TYPE = {
+  ITEM_TYPES = {
     "aged" => MaturingItem,
     "concert" => DeadlineItem,
     "sulfuras" => LegendaryItem,
     "conjured" => ConjuredItem
   }
 
-  attr_reader :items
+  attr_reader :updated_items
 
   def initialize(items)
     @items = items
+    @updated_items = []
+    @item_type = BaseItem
   end
 
   def update_quality
-    @items.map! do |item|
-      item = BaseItem.new(item.name, item.sell_in, item.quality)
-      ITEM_TYPE.each do |key, value|
-        item = value.new(item.name, item.sell_in, item.quality) if item.name.downcase.include? key
+    @items.each do |item|
+      ITEM_TYPES.each do |key, value|
+         @item_type = value if item.name.downcase.include? key
       end
-      item.update_item
+      item = @item_type.new(item.name, item.sell_in, item.quality)
+      @updated_items.push(item.update_item)
     end
-    self
+    @updated_items
   end
 
 end
